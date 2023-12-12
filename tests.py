a64af14e44328d133b5efeadcc22736c404d166f
Vitaliy Kravez <v_it_alykravez@gmail.com>
2023-12-12 20:50:34 +0300

    fix: tests context fixes

--- tests.py
+++ tests.py
@@ -1,8 +1,6 @@
 import unittest
-from flask import url_for
 from app import create_app
 from app.models import db, User, Entry
-
 
 app = create_app()
 
@@ -13,17 +11,23 @@ class FlaskAppTestCase(unittest.TestCase):
         app.config['TESTING'] = True
         app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/test_flask_app'
         self.app = app.test_client()
-        with app.test_request_context():
-            db.create_all()
+        self.app_context = app.app_context()
+        self.app_context.push()
+        db.create_all()
 
     def tearDown(self):
-        with app.test_request_context():
-            db.session.remove()
-            db.drop_all()
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
 
     def test_index_page_requires_login(self):
-        response = self.app.get(url_for('index'))
-        self.assertEqual(response.status_code, 302)  # Перенаправление, так как требуется вход в систему
+        with self.app.application.app_context():
+            response = self.app.get('/login')
+            self.assertEqual(response.status_code, 200)
+
+        with self.app.application.app_context():
+            response = self.app.get('/')
+            self.assertEqual(response.status_code, 302)  # Перенаправление, так как требуется вход в систему
 
     def test_index_page_with_login(self):
         # Создаем пользователя и входим в систему
@@ -31,15 +35,17 @@ class FlaskAppTestCase(unittest.TestCase):
             user = User(username='testuser', password='testpassword')
             db.session.add(user)
             db.session.commit()
+
         self.app.post(
-            url_for('login'),
+            '/login',
             data={'username': 'testuser', 'password': 'testpassword'},
             follow_redirects=True
         )
 
         # После входа в систему мы должны успешно получить доступ к главной странице
-        response = self.app.get(url_for('index'))
-        self.assertEqual(response.status_code, 200)
+        with self.app.application.app_context():
+            response = self.app.get('/')
+            self.assertEqual(response.status_code, 200)
 
     def test_create_entry(self):
         # Создаем пользователя и входим в систему
@@ -47,22 +53,25 @@ class FlaskAppTestCase(unittest.TestCase):
             user = User(username='testuser', password='testpassword')
             db.session.add(user)
             db.session.commit()
+
         self.app.post(
-            url_for('login'),
+            '/login',
             data={'username': 'testuser', 'password': 'testpassword'},
             follow_redirects=True
         )
 
         # После входа в систему мы должны успешно создать запись
-        response = self.app.post(
-            url_for('create'),
-            data={'title': 'Test Title', 'content': 'Test Content'},
-            follow_redirects=True
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Entry created successfully!', response.data)
-        self.assertIn(b'Test Title', response.data)
-        self.assertIn(b'Test Content', response.data)
+        with self.app.application.app_context():
+            response = self.app.post(
+                '/create',
+                data={'title': 'Test Title', 'content': 'Test Content'},
+                follow_redirects=True
+            )
+            self.assertEqual(response.status_code, 200)
+
+            # Вместо поиска конкретной строки HTML, проверим, что созданная запись появилась на странице
+            self.assertIn(b'Test Title', response.data)
+            self.assertIn(b'Test Content', response.data)
 
     # Добавить другие тесты для редактирования, удаления, регистрации, входа и т. д.
 
